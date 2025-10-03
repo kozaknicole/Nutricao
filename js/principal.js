52346cd8e74cceb0d3aaec13bb34a8b2bd3b8b6e
Nicole Kozak <nicole.kozak@escola.pr.gov.br>
2025-10-03 11:08:23 -0300

    uk

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,10 @@
 var titulo = document.querySelector("h1");
-
 console.log(titulo);
-
 console.log(titulo.textContent);
-
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente=document.querySelector("#primeiro-paciente");
-
-console.log(paciente)
-
 var tdPeso = paciente.querySelector(".info-peso");
-console.log(tdPeso)
+var peso = tdPeso.textContent
+console.log(paciente)
+console.log(peso)
