7af98dd9670518c55d683de83bb4594eb5430033
Nicole Kozak <nicole.kozak@escola.pr.gov.br>
2025-10-07 09:00:17 -0300

    jnkjbn

--- js/principal.js
+++ js/principal.js
@@ -8,3 +8,7 @@ console.log(paciente);
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(peso);
+
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+console.log (altura);
